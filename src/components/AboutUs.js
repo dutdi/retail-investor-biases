e9afa984e8a6d0ad9720be6b78113a34d6d4e3e5
Tunar Mahmudov <tunar.mahmudov@tum.de>
2021-12-02 00:56:00 +0100

    start about us contact us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,25 @@
+import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles({
+  root: {
+    width: '1000px',
+    height: '500px',
+    margin: '100px',
+    backgroundColor: 'white',
+    textAlign: 'center',
+  },
+});
+
 const AboutUs = () => {
-  return <div>About us</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Typography variant='h4' gutterBottom>
+        About us
+      </Typography>
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,7 +1,25 @@
+import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles({
+  root: {
+    width: '1000px',
+    height: '500px',
+    margin: '100px',
+    backgroundColor: 'white',
+    textAlign: 'center',
+  },
+});
+
 const ContactUs = () => {
-  return <div>Contact us</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Typography variant='h4' gutterBottom>
+        Contact us
+      </Typography>
+    </div>
+  );
 };
 
 export default ContactUs;
