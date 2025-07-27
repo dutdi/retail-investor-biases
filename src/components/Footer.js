b1c4fab0c6a87e1bb01b314c1f86e2330783fddc
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-07-27 22:11:11 +0400

    make footer flexable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   footerRoot: {
-    display: 'flex',
-    position: 'absolute',
+    display: "flex",
+    flexDirection: "column",
+    height: "100px",
+    /*position: "absolute" */
     left: 0,
     bottom: 0,
     right: 0,
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2),
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
 }));
 
@@ -23,38 +25,38 @@ const Footer = () => {
     <div className={classes.footerRoot}>
       <Typography
         style={{
-          margin: '0 auto',
-          textAlign: 'center',
+          margin: "0 auto",
+          textAlign: "center",
         }}
-        variant='body1'
+        variant="body1"
         gutterBottom
       >
         Made with 💙 in Munich by Tunar Mahmudov & Muneeb Ahmed @ TUM's Chair of
         Digital Finance
       </Typography>
-      <div style={{ position: 'absolute', right: '0px' }}>
+      <div style={{ textAlign: "center", right: "0px" }}>
         <Typography
           component={Link}
-          to='/imprint'
-          variant='h6'
-          style={{ color: '#0065bd', textDecoration: 'none' }}
+          to="/imprint"
+          variant="h6"
+          style={{ color: "#0065bd", textDecoration: "none" }}
         >
           Imprint
         </Typography>
         <Typography
           component={Link}
-          to='/privacy'
-          variant='h6'
-          style={{ color: '#0065bd', textDecoration: 'none' }}
+          to="/privacy"
+          variant="h6"
+          style={{ color: "#0065bd", textDecoration: "none" }}
         >
-          {' '}
-          | Privacy |{' '}
+          {" "}
+          | Privacy |{" "}
         </Typography>
         <Typography
           component={Link}
-          to='/contact-us'
-          variant='h6'
-          style={{ color: '#0065bd', textDecoration: 'none' }}
+          to="/contact-us"
+          variant="h6"
+          style={{ color: "#0065bd", textDecoration: "none" }}
         >
           Contact us
         </Typography>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import Image from '../img/landing.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import Image from "../img/landing.png";
 
 const useStyles = makeStyles({
   image: {
     backgroundImage: `url(${Image})`,
-    backgroundSize: 'contain',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center center',
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
   },
   text1: {
-    textAlign: 'center',
-    color: 'black',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
+    textAlign: "center",
+    color: "black",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
   },
   text2: {
-    textAlign: 'center',
-    color: '#0065bd',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    fontWeight: '600',
+    textAlign: "center",
+    color: "#0065bd",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    fontWeight: "600",
   },
   text3: {
-    textAlign: 'center',
-    color: 'black',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    fontStyle: 'italic',
+    textAlign: "center",
+    color: "black",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    fontStyle: "italic",
   },
 });
 
@@ -43,26 +43,26 @@ const Landing = () => {
 
   return (
     <Box m={5}>
-      <Typography className={classes.text1} variant='h5' gutterBottom>
+      <Typography className={classes.text1} variant="h5" gutterBottom>
         The Only Tool You Need to
       </Typography>
-      <Typography className={classes.text2} variant='h2' gutterBottom>
+      <Typography className={classes.text2} variant="h2" gutterBottom>
         Find & Combat Your Biases
       </Typography>
-      <Box textAlign='center' mt={10}>
-        <Typography className={classes.text3} variant='h6' gutterBottom>
+      <Box textAlign="center" mt={10}>
+        <Typography className={classes.text3} variant="h6" gutterBottom>
           by TUM Chair of Digital Finance
         </Typography>
         <Button
           component={Link}
-          to='/study-info'
-          variant='contained'
-          style={{ backgroundColor: '#0065bd', color: 'white' }}
+          to="/study-info"
+          variant="contained"
+          style={{ backgroundColor: "#0065bd", color: "white" }}
         >
           Get Started
         </Button>
         <Box className={classes.image} mt={10}>
-          <img alt='background' src={Image} />
+          <img alt="background" src={Image} />
         </Box>
       </Box>
     </Box>
