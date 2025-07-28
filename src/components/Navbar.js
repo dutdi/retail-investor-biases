aaceb8cc939f4b2ce8b80fb9421212417d2d9e2c
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-07-28 17:51:47 +0400

    make response navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Image from '../img/digital finance.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Image from "../img/digital finance.png";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   appBar: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   title: {
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    textAlign: 'center',
-    color: '#0065bd',
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    textAlign: "center",
+    color: "#0065bd",
     padding: 5,
-    textDecoration: 'none',
-    boxShadow: 'none',
+    textDecoration: "none",
+    boxShadow: "none",
+    fontSize: "2.5rem",
+    [theme.breakpoints.up("md")]: {
+      fontSize: "3rem",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "3.5rem",
+    },
   },
 }));
 
@@ -30,19 +37,19 @@ const Navbar = () => {
 
   return (
     <div className={classes.root}>
-      <AppBar className={classes.appBar} position='static'>
+      <AppBar className={classes.appBar} position="static">
         <Toolbar>
-          <Link to='/'>
+          <Link to="/">
             <img
-              style={{ width: '70px', height: '70px' }}
-              alt='logo'
+              style={{ width: "70px", height: "70px" }}
+              alt="logo"
               src={Image}
             />
           </Link>
           <Typography
             component={Link}
-            to='/'
-            variant='h4'
+            to="/"
+            variant="h4"
             className={classes.title}
           >
             Retail Investor Biases
