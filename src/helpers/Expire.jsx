f003d5c9eb75a9464ab45826b0902ebcfa658deb
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-08-13 10:51:37 +0400

    bug fix

--- src/helpers/Expire.jsx
+++ src/helpers/Expire.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const Expire = ({ setShowWrongAnswer, delay, children }) => {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setVisible(false);
       setShowWrongAnswer(false);
     }, delay);
+
+    // Cleanup on unmount
+    return () => clearTimeout(timer);
   }, [delay, setShowWrongAnswer]);
 
-  return visible ? <div>{children}</div> : <div></div>;
+  return visible ? <div>{children}</div> : null;
 };
 
 export default Expire;
